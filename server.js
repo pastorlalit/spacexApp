fd7ee3885a69e690e7d97a4e02e9738021b44893
pastorlalit <pastorlalit1989@gmail.com>
2019-11-08 02:58:35 +0530

    Finished App

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express'); // import graphql
+
+const cors = require('cors');
+
+const graphqlHTTP = require('express-graphql');  // import express graphql module
+
+const schema = require('./schema');
+
+const app = express(); // initialize express
+
+// Allow cross-origin
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
